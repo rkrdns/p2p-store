fa948ac7a46909a4ff101a00d5d394ea002df042
Neiber Cardenas <rebienkrdns@outlook.com>
2022-10-03 19:20:18 -0500

    initial commit: view, controller, model and base migration

---
+++ resources/views/store.blade.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="author" content="Neiber Cardenas" />
+  <meta name="description" content="Neiber Cardenas Store" />
+  <meta name="keywords" content="Store">
+
+  <title>Neiber Cardenas Store</title>
+
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+  <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap" rel="stylesheet">
+
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/flaticon.css" rel="stylesheet">
+  <link href="css/menu.css" rel="stylesheet">
+  <link id="effect" href="css/dropdown-effects/fade-down.css" media="all" rel="stylesheet">
+  <link href="css/magnific-popup.css" rel="stylesheet">
+  <link href="css/owl.carousel.min.css" rel="stylesheet">
+  <link href="css/owl.theme.default.min.css" rel="stylesheet">
+  <link href="css/animate.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/responsive.css" rel="stylesheet">
+</head>
+
+<body>
+  <div id="loading" class="skyblue-loading">
+    <div id="loading-center">
+      <div id="loading-center-absolute">
+        <div class="object" id="object_one"></div>
+        <div class="object" id="object_two"></div>
+        <div class="object" id="object_three"></div>
+        <div class="object" id="object_four"></div>
+      </div>
+    </div>
+  </div>
+  <div id="page" class="page">
+    <header id="header" class="header white-menu navbar-dark">
+      <div class="header-wrapper">
+        <div class="wsmobileheader clearfix">
+          <span class="smllogo"><img src="images/store.png" alt="mobile-logo" /></span>
+          <a id="wsnavtoggle" class="wsanimated-arrow"><span></span></a>
+        </div>
+        <div class="wsmainfull menu clearfix">
+          <div class="wsmainwp clearfix">
+            <div class="desktoplogo"><a href="demo-1.html" class="logo-black"><img src="images/store.png" alt="header-logo"></a></div>
+            <div class="desktoplogo"><a href="demo-1.html" class="logo-white"><img src="images/logo-white.png" alt="header-logo"></a></div>
+            <nav class="wsmenu clearfix">
+              <ul class="wsmenu-list nav-skyblue-hover">
+                <li class="nl-simple" aria-haspopup="true"><a href="https://www.linkedin.com/in/rebienkrdns/">Linkedin</a></li>
+                <li class="nl-simple" aria-haspopup="true"><a href="https://github.com/rebienkrdns">Github</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <section id="pricing-2" class="bg-snow pb-60 inner-page-hero pricing-section division">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-10 col-xl-8">
+            <div class="section-title title-01 mb-40">
+              <h2 class="h2-md">Neiber Cardenas Store</h2>
+            </div>
+          </div>
+        </div>
+        <div class="pricing-2-row pc-25">
+          <div class="row row-cols-1 row-cols-md-3">
+            <div class="col">
+              <div class="pricing-2-table bg-white mb-40 wow fadeInUp">
+                <div class="pricing-plan">
+                  <div class="row">
+                    <div class="col">
+                      <img src="images/pc.png" class="img-fluid">
+                    </div>
+                  </div>
+                  <div class="pricing-plan-title">
+                    <h5 class="h5-xs">PC</h5>
+                    <h6 class="h6-sm bg-lightgrey">Save 30%</h6>
+                  </div>
+                  <sup class="dark-color">$</sup>
+                  <span class="dark-color">2</span>
+                  <sup class="validity dark-color"><span>.000.000</span></sup>
+                </div>
+                <a href="#" class="btn btn-sm btn-tra-grey tra-skyblue-hover">Comprar</a>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="pricing-2-table bg-white mb-40 wow fadeInUp">
+                <div class="pricing-plan">
+                  <div class="row">
+                    <div class="col">
+                      <img src="images/printer.png" class="img-fluid">
+                    </div>
+                  </div>
+                  <div class="pricing-plan-title">
+                    <h5 class="h5-xs">Printer</h5>
+                    <h6 class="h6-sm bg-lightgrey">Save 10%</h6>
+                  </div>
+                  <sup class="dark-color">$</sup>
+                  <span class="dark-color">5</span>
+                  <sup class="validity dark-color"><span>00.000</span></sup>
+                </div>
+                <a href="#" class="btn btn-sm btn-tra-grey tra-skyblue-hover">Comprar</a>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="pricing-2-table bg-white mb-40 wow fadeInUp">
+                <div class="pricing-plan">
+                  <div class="row">
+                    <div class="col">
+                      <img src="images/laptop.png" class="img-fluid">
+                    </div>
+                  </div>
+                  <div class="pricing-plan-title">
+                    <h5 class="h5-xs">Laptop</h5>
+                    <h6 class="h6-sm bg-lightgrey">Save 25%</h6>
+                  </div>
+                  <sup class="dark-color">$</sup>
+                  <span class="dark-color">3</span>
+                  <sup class="validity dark-color"><span>.500.000</span></sup>
+                </div>
+                <a href="#" class="btn btn-sm btn-tra-grey tra-skyblue-hover">Comprar</a>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer id="footer-1" class="footer division">
+      <div class="container">
+        <div class="bottom-footer">
+          <div class="row row-cols-1 row-cols-md-2 d-flex align-items-center">
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+  <script src="js/jquery-3.6.0.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/modernizr.custom.js"></script>
+  <script src="js/jquery.easing.js"></script>
+  <script src="js/jquery.appear.js"></script>
+  <script src="js/jquery.scrollto.js"></script>
+  <script src="js/menu.js"></script>
+  <script src="js/imagesloaded.pkgd.min.js"></script>
+  <script src="js/isotope.pkgd.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/quick-form.js"></script>
+  <script src="js/request-form.js"></script>
+  <script src="js/jquery.validate.min.js"></script>
+  <script src="js/jquery.ajaxchimp.min.js"></script>
+  <script src="js/wow.js"></script>
+  <script src="js/custom.js"></script>
+</body>
+
+</html>
